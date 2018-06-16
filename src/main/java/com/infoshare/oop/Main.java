7788960d50a84e94b4652e9ecbd1a9c14349f86b
Michał Mojek <m.mojek2@gmail.com>
2018-06-16 12:15:43 +0200

    first commit

--- src/main/java/com/infoshare/oop/Main.java
+++ src/main/java/com/infoshare/oop/Main.java
@@ -2,6 +2,7 @@ package com.infoshare.oop;
 
 import com.infoshare.oop.card.Card;
 import com.infoshare.oop.card.CreditCard;
+import com.infoshare.oop.card.DebitCard;
 
 import java.math.BigDecimal;
 
@@ -10,6 +11,7 @@ public class Main {
         Card card = new Card("Karta", "1234");
         CreditCard creditCard = new CreditCard("Karta kredytowa", "123", BigDecimal.ONE, new BigDecimal(200));
 
+        DebitCard debitCard = new DebitCard("name", "number")
         card.pay();
         creditCard.pay();
 
