52f4b01963c9d99e364c84909d3c8bb5e3228331
krzysztofpodlaski <krzysztof.podlaski@yahoo.pl>
2018-06-16 09:00:19 +0200

    First changes - new class Card added.

---
+++ src/main/java/com/infoshare/oop/Main.java
@@ -0,0 +1,9 @@
+package com.infoshare.oop;
+
+public class Main {
+    public static void main (String[] args ) {
+
+        System.out.println("Start!");
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/infoshare/oop/com/infoshare/oop/card/Card.java
@@ -0,0 +1,11 @@
+package com.infoshare.oop.com.infoshare.oop.card;
+
+public class Card {
+    protected String CardName;
+
+    public Card(String CardName) {
+        this.CardName = "Visa";
+    }
+
+
+}
