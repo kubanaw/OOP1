37d8dd2ae0dd5dcf7c927ad312c5e8cdafc6e178
NWelter <nataliawelter@wp.pl>
2018-06-16 08:51:44 +0200

    initial commit

---
+++ src/main/java/com/infoshare/oop/card/Card.java
@@ -0,0 +1,17 @@
+package com.infoshare.oop.card;
+
+public class Card {
+
+    //fields:
+
+    protected String name;
+    protected String cardNumber;
+
+    //constructor:
+
+    public Card (String name, String cardNumber){
+        this.name = name;
+        this.cardNumber = cardNumber;
+    }
+
+}
---
+++ src/main/java/com/infoshare/oop/card/Main.java
@@ -0,0 +1,7 @@
+package com.infoshare.oop.card;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
